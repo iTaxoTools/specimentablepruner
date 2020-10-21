dc2e550651c1a08243df09d0393d07f3d337e915
Vladimir Kharchev <necrosovereign@gmail.com>
2020-10-21 17:41:13 +0200

    Copied ScrolledText from DNAConvert

--- library/gui_utils.py
+++ library/gui_utils.py
@@ -42,7 +42,7 @@ class FileOrDirChooser(FileChooser):
     Adds a button to choose a directory to FileChooser
     """
 
-    def __init__(self, parent, *, label: str):
+    def __init__(self, parent: tk.Widget, *, label: str):
         super().__init__(parent, label=label, mode="open")
 
         def browse_dir() -> None:
@@ -88,4 +88,28 @@ class LabeledCombobox():
         self.frame.columnconfigure(1, weight=1)
         self.label.grid(column=0, row=0)
         self.combobox.grid(column=1, row=0)
+        self.grid = self.frame.grid
+
+
+class ScrolledText():
+    """
+    A text input widget with two scrollbars
+    """
+
+    def __init__(self, parent: tk.Widget, *, label: str) -> None:
+        self.frame = ttk.Frame(parent)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
+        self.label = ttk.Label(self.frame, text=label)
+        self.text = tk.Text(self.frame, width=50, height=20)
+        self.vbar = ttk.Scrollbar(
+            self.frame, orient=tk.VERTICAL, command=self.text.yview)
+        self.hbar = ttk.Scrollbar(
+            self.frame, orient=tk.HORIZONTAL, command=self.text.xview)
+        self.text.configure(xscrollcommand=self.hbar.set,
+                            yscrollcommand=self.vbar.set)
+        self.label.grid(row=0, column=0, sticky='w')
+        self.text.grid(row=1, column=0, sticky='nsew')
+        self.vbar.grid(row=1, column=1, sticky='nsew')
+        self.hbar.grid(row=2, column=0, sticky='nsew')
         self.grid = self.frame.grid
