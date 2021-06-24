a67e4fcd218c946281a9c852a30b5787c4634919
sangeeta97 <kumarisangeeta097@gmail.com>
2021-06-24 18:54:48 +0200

    pyinstaller_version

---
+++ library/gui_utils.py
@@ -0,0 +1,197 @@
+import io
+import os.path, sys
+import tkinter as tk
+import tkinter.filedialog as tkfiledialog
+import tkinter.ttk as ttk
+from typing import Any, Dict, Iterator, List, TextIO, Optional
+
+
+resource_path = getattr(sys, '_MEIPASS', sys.path[0])
+
+class FileChooser():
+    """
+    Creates a frame with a label, entry and browse button for choosing files
+    """
+
+    def __init__(self, parent: Any, *, label: str, mode: str):
+        self.frame = ttk.Frame(parent)
+        self.frame.columnconfigure([0, 1], weight=1)
+        self.label = ttk.Label(self.frame, text=label)
+        self.file_var = tk.StringVar()
+        self.entry = ttk.Entry(self.frame, textvariable=self.file_var)
+        if mode == "open":
+            self._dialog = tkfiledialog.askopenfilename
+        elif mode == "save":
+            self._dialog = tkfiledialog.asksaveasfilename
+        else: # assume "open"
+            self._dialog = tkfiledialog.askopenfilename
+
+        def browse() -> None:
+            newpath: Optional[str] = self._dialog()
+            if newpath:
+                try:
+                    newpath = os.path.relpath(newpath)
+                except:
+                    newpath = os.path.abspath(newpath)
+                self.file_var.set(newpath)
+
+        self.button = ttk.Button(self.frame, text="Browse", command=browse)
+
+        self.label.grid(row=0, column=0, sticky='nws')
+        self.entry.grid(row=1, column=0, sticky='nwse')
+        self.button.grid(row=1, column=1)
+        self.grid = self.frame.grid
+
+
+class FileOrDirChooser(FileChooser):
+    """
+    Adds a button to choose a directory to FileChooser
+    """
+
+    def __init__(self, parent: tk.Misc, *, label: str):
+        super().__init__(parent, label=label, mode="open")
+
+        def browse_dir() -> None:
+            newpath: Optional[str] = tkfiledialog.askdirectory()
+            if newpath:
+                try:
+                    newpath = os.path.relpath(newpath)
+                except:
+                    newpath = os.path.abspath(newpath)
+                self.file_var.set(newpath)
+
+        self.dir_button = ttk.Button(
+            self.frame, text="Browse dir", command=browse_dir)
+        self.dir_button.grid(row=2, column=1)
+
+
+class LabeledEntry():
+    """
+    Group of a label, entry and a string variable
+    """
+
+    def __init__(self, parent: tk.Misc, *, label: str):
+        self.frame = ttk.Frame(parent)
+        self.label = ttk.Label(self.frame, text=label)
+        self.var = tk.StringVar()
+        self.entry = ttk.Entry(self.frame, textvariable=self.var)
+        self.frame.columnconfigure(1, weight=1)
+        self.label.grid(column=0, row=0)
+        self.entry.grid(column=1, row=0)
+        self.grid = self.frame.grid
+
+
+class LabeledCombobox():
+    """
+    Group of a label, combobox and a string variable
+    """
+
+    def __init__(self, parent: tk.Misc, *, label: str, values: List[str]):
+        self.frame = ttk.Frame(parent)
+        self.label = ttk.Label(self.frame, text=label)
+        self.var = tk.StringVar()
+        self.combobox = ttk.Combobox(
+            self.frame, textvariable=self.var, values=values)
+        self.frame.columnconfigure(1, weight=1)
+        self.label.grid(column=0, row=0)
+        self.combobox.grid(column=1, row=0)
+        self.grid = self.frame.grid
+
+
+class ScrolledText():
+    """
+    A text input widget with two scrollbars
+    """
+
+    def __init__(self, parent: tk.Misc, *, label: str, width: int, height: int) -> None:
+        self.frame = ttk.Frame(parent)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
+        self.label = ttk.Label(self.frame, text=label)
+        self.text = tk.Text(self.frame, width=width, height=height)
+        self.vbar = ttk.Scrollbar(
+            self.frame, orient=tk.VERTICAL, command=self.text.yview)
+        self.hbar = ttk.Scrollbar(
+            self.frame, orient=tk.HORIZONTAL, command=self.text.xview)
+        self.text.configure(xscrollcommand=self.hbar.set,
+                            yscrollcommand=self.vbar.set)
+        self.label.grid(row=0, column=0, sticky='w')
+        self.text.grid(row=1, column=0, sticky='nsew')
+        self.vbar.grid(row=1, column=1, sticky='nsew')
+        self.hbar.grid(row=2, column=0, sticky='nsew')
+        self.grid = self.frame.grid
+
+    def get_text(self) -> str:
+        return self.text.get('1.0', 'end')
+
+
+class FilesOrText():
+    """
+    Two tabs: one for choosing a file, another for inputting text
+    """
+
+    def __init__(self, parent: tk.Misc, *, file_label: str, text_label: str, width: int, height: int, mode: str) -> None:
+        """
+        width and height are the parameters of the textbox
+        file_label is shown at the file name entry
+        text_label is shown at the textbox
+        """
+        self.notebook = ttk.Notebook(parent)
+        if mode == 'opendir':
+            self.filechooser: FileChooser = FileOrDirChooser(
+                self.notebook, label=file_label)
+        else:
+            self.filechooser = FileChooser(
+                self.notebook, label=file_label, mode=mode)
+        self.textbox = ScrolledText(
+            self.notebook, label=text_label, width=width, height=height)
+        self.notebook.add(self.filechooser.frame, text=file_label)
+        self.notebook.add(self.textbox.frame, text=text_label)
+        self.grid = self.notebook.grid
+
+    def text(self) -> Iterator[TextIO]:
+        if self.notebook.index('current') == 0:
+            path = self.filechooser.file_var.get()
+            if os.path.isdir(path):
+                for entry in os.listdir(path):
+                    with open(entry.path, errors='replace') as file:
+                        yield file
+            else:
+                with open(path, errors='replace') as file:
+                    yield file
+        elif self.notebook.index('current') == 1:
+            yield io.StringIO(self.textbox.get_text())
+
+    def is_file(self) -> bool:
+        return self.notebook.index('current') == 0
+
+    def is_text(self) -> bool:
+        return self.notebook.index('current') == 1
+
+    def text_contents(self) -> str:
+        return self.textbox.get_text()
+
+    def file_name(self) -> str:
+        return self.filechooser.file_var.get()
+
+
+class RadioGroup():
+    """
+    Generates a group of exclusive radiobuttons
+    """
+
+    def __init__(self, parent: tk.Misc, *, label: str, values: Dict[str, str], direction: str) -> None:
+        self.frame = ttk.Frame(parent)
+        self.label = ttk.Label(self.frame, text=label)
+        self.subframe = ttk.Frame(self.frame, relief='sunken', padding=5)
+        self.var = tk.StringVar()
+        self.radiobuttons = []
+        for name, value in values.items():
+            self.radiobuttons.append(ttk.Radiobutton(
+                self.subframe, text=name, variable=self.var, value=value))
+        dx, dy = (1, 0) if direction == 'horizontal' else (0, 1)
+        for i in range(len(values)):
+            self.radiobuttons[i].grid(row=i*dy, column=i*dx, sticky="w")
+        self.label.grid(row=0, column=0, sticky="w")
+        self.subframe.grid(row=1, column=0, sticky="nsew")
+        self.grid = self.frame.grid
---
+++ specimentablepruner.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+from library.gui_utils import *
+import pandas as pd
+import tkinter.messagebox as tkmessagebox
+import tkinter.font as tkfont
+from typing import Set
+import re
+import sys
+
+format_dict = {
+    "tab": '\t',
+    "CSV (comma delimited)": ',',
+    "CSV (semicolon delimited)": ';'
+}
+
+fuzzy_pruning_regex = re.compile(r'[- _./\\]+')
+
+resource_path = getattr(sys, '_MEIPASS', sys.path[0])
+
+
+
+class Pruner():
+
+    def __init__(self) -> None:
+        self.fuzzy_pruning = False
+
+    def set_input_sep(self, sep: str) -> None:
+        self.input_sep = sep
+
+    def set_output_sep(self, sep: str) -> None:
+        self.output_sep = sep
+
+    def set_pruning_field(self, field: str) -> None:
+        self.pruning_field = field
+
+    def pruning_from_file(self, file: TextIO) -> None:
+        try:
+            column = pd.read_csv(file, sep=self.input_sep, usecols=[
+                                 self.pruning_field], squeeze=True)
+        except ValueError:
+            raise ValueError(f"Pruning file {file.name} seems to be missing {self.pruning_field}")
+        self.pruning_values: Set[str] = set(column.unique())
+
+    def pruning_from_str(self, text: str) -> None:
+        lines = text.split("\n")
+        if len(lines) == 1:
+            pruning_values = lines[0].split(',')
+        else:
+            pruning_values = lines
+        self.pruning_values = set(pruning_values)
+
+    def set_fuzzy_pruning(self) -> None:
+        self.fuzzy_pruning = True
+        self.pruning_values = {fuzzy_pruning_regex.sub("", s.casefold()) for s in self.pruning_values}
+
+
+    def set_files(self, infile: TextIO, backup_outname: str) -> None:
+        self.input_file = infile
+        try:
+            input_name = infile.name
+        except AttributeError:
+            output_name = backup_outname
+        else:
+            base, ext = os.path.splitext(input_name)
+            output_name = base + "_pruned" + ext
+        self.output_file = open(output_name, mode='w', newline='', errors='replace')
+
+    def prune(self) -> None:
+        table = pd.read_csv(self.input_file, sep=self.input_sep)
+        try:
+            pruning_column = table[self.pruning_field]
+        except KeyError as ex:
+            raise ValueError(f"Input file doesn't contain column {self.pruning_field}") from ex
+        else:
+            if self.fuzzy_pruning:
+                pruning_column = pruning_column.str.casefold().replace(fuzzy_pruning_regex, "")
+            table = table.loc[~pruning_column.isin(
+                self.pruning_values)]
+        table.to_csv(self.output_file, sep=self.output_sep, index=False)
+
+
+def gui_main() -> None:
+    root = tk.Tk()
+    root.title("Specimentablepruner")
+    if os.name == "nt":
+        root.wm_iconbitmap(os.path.join(resource_path, 'data', 'specimentablepruner_icon.ico'))
+
+    stype = ttk.Style()
+    stype.configure(style="PruneButton.TButton", background="blue")
+
+    banner_frame = ttk.Frame(root)
+    logo_img = tk.PhotoImage(file=os.path.join(resource_path, 'data', 'iTaxoTools Digital linneaeus MICROLOGO.png'))
+    ttk.Label(banner_frame, image=logo_img).grid(row=0, column=0, sticky="nsw")
+    ttk.Label(banner_frame, text="Tool to prune rows from tables based on lists of specimen identifiers or species names", font = tkfont.Font(size=14)).grid(row=0, column=2)
+
+    input_widget = FilesOrText(
+        root, file_label="Input file", text_label="Input_text", width=35, height=10, mode='opendir')
+    output_file_chooser = FileChooser(
+        input_widget.textbox.frame, label="Output file", mode="save")
+    output_file_chooser.grid(row=3, column=0)
+
+    prune_widget = FilesOrText(root, file_label="Prune file",
+                               text_label="Prune values", width=35, height=10, mode='open')
+
+    prune_field_cmb = LabeledCombobox(root, label="Field to prune", values=[
+        "specimenid", "species", "voucher", "locality"])
+    prune_field_cmb.var.set("specimenid")
+
+    fuzzy_pruning_var = tk.BooleanVar()
+    fuzzy_pruning_chk = ttk.Checkbutton(root, text="Allow for common misspellings of prune value", variable=fuzzy_pruning_var)
+
+    bottom_frame = ttk.Frame(root)
+    input_format_chooser = RadioGroup(
+        bottom_frame, label="Format of the input file", values=format_dict, direction='vertical')
+    input_format_chooser.var.set('\t')
+    output_format_chooser = RadioGroup(
+        bottom_frame, label="Format of the output file", values=format_dict, direction='vertical')
+    output_format_chooser.var.set('\t')
+
+    pruner = Pruner()
+
+    def prune() -> None:
+        try:
+            pruner.set_input_sep(input_format_chooser.var.get())
+            pruner.set_output_sep(output_format_chooser.var.get())
+
+            pruner.set_pruning_field(prune_field_cmb.var.get())
+
+            if prune_widget.is_file():
+                with open(prune_widget.file_name(), errors='replace') as prune_file:
+                    pruner.pruning_from_file(prune_file)
+            elif prune_widget.is_text():
+                pruner.pruning_from_str(prune_widget.text_contents())
+
+            if fuzzy_pruning_var.get():
+                pruner.set_fuzzy_pruning()
+
+            backup_outfile_name = output_file_chooser.file_var.get()
+
+            for infile in input_widget.text():
+                pruner.set_files(infile, backup_outfile_name)
+                pruner.prune()
+                pruner.output_file.close()
+        except Exception as ex:
+            tkmessagebox.showerror("Error", str(ex))
+            raise Exception from ex
+        else:
+            tkmessagebox.showinfo("Done", "Pruning is complete")
+
+    prune_btn = ttk.Button(bottom_frame, text="Prune", command=prune, style="PruneButton.TButton")
+
+    banner_frame.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+    ttk.Separator(root, orient='horizontal').grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+    input_widget.grid(row=2, column=0, sticky="nsew", padx=5, pady=5)
+    prune_field_cmb.grid(row=3, column=0, columnspan=2)
+    prune_widget.grid(row=2, column=1, sticky="nsew", padx=5, pady=5)
+
+    fuzzy_pruning_chk.grid(row=4, column=0, columnspan=2)
+
+    input_format_chooser.grid(row=2, column=0)
+    prune_btn.grid(row=2, column=1, padx=10)
+    output_format_chooser.grid(row=2, column=2)
+    bottom_frame.grid(row=5, column=0, columnspan=2)
+
+    root.rowconfigure(2, weight=1)
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=1)
+
+    root.mainloop()
+
+
+def main() -> None:
+    gui_main()
+
+
+if __name__ == "__main__":
+    main()
